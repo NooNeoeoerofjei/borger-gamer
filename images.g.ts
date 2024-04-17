8506753f31e6dca664048e4e25e5477cc42dad59
NooNeoeoerofjei <123140418+NooNeoeoerofjei@users.noreply.github.com>
2024-04-17 12:15:34 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,82 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image2":
+            case "myImage0":return img`
+. . . . e e e e e e e . . . . . 
+. . . e d d d d d d d e . . . . 
+. . . d d f d d d f d d . . . . 
+. . . d d d d d d d d d . . . . 
+. . . d d d d d d d d d . . . . 
+. . . . d d d d d d d . . . . . 
+. . . . . 4 4 4 4 4 . . . . . . 
+. . . . 4 4 4 4 4 4 4 . . . . . 
+. . . . 4 4 4 4 4 4 4 . . . . . 
+. . . . 4 4 4 4 4 4 4 . . . . . 
+. . . . 4 4 4 4 4 4 4 . . . . . 
+. . . . 4 4 4 4 4 4 4 . . . . . 
+. . . . d 4 4 4 4 4 d . . . . . 
+. . . . . 8 8 . 8 8 . . . . . . 
+. . . . . 8 8 . 8 8 . . . . . . 
+. . . . . 8 8 . 8 8 . . . . . . 
+`;
+            case "image3":
+            case "myImage1":return img`
+. . . . . e e e e e e . . . . . 
+. . . . e d d d d d d e . . . . 
+. . . . e d f d d f d e . . . . 
+. . . . d d d d d d d d . . . . 
+. . . . d d d d d d d d . . . . 
+. . . . . d d d d d d . . . . . 
+. . . . . f f f f f f . . . . . 
+. . . . f f f f f f f f . . . . 
+. . . . f f f f f f f f . . . . 
+. . . . f f f f f f f f . . . . 
+. . . . f f f f f f f f . . . . 
+. . . . d f f f f f f d . . . . 
+. . . . . 6 6 6 6 6 6 . . . . . 
+. . . . . 6 6 . . 6 6 . . . . . 
+. . . . . 6 6 . . 6 6 . . . . . 
+. . . . . e e . . e e . . . . . 
+`;
+            case "image1":
+            case "bald cheese":return img`
+. . . . . d d d d d d . . . . . 
+. . . . d f d d d d f d . . . . 
+. . . . d d d d d d d d . . . . 
+. . . . d d d d d d d d . . . . 
+. . . . d d 2 2 2 2 d d . . . . 
+. . . . . d d d d d d . . . . . 
+. . . . . 7 7 7 7 7 7 . . . . . 
+. . . . 7 7 7 7 7 7 7 7 . . . . 
+. . . . 7 7 7 7 7 7 7 7 . . . . 
+. . . . 7 7 7 7 7 7 7 7 . . . . 
+. . . . 7 7 7 7 7 7 7 7 . . . . 
+. . . . d 7 7 7 7 7 7 d . . . . 
+. . . . . 6 6 6 6 6 6 . . . . . 
+. . . . . 6 6 . . 6 6 . . . . . 
+. . . . . 6 6 . . 6 6 . . . . . 
+. . . . . e e . . e e . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
